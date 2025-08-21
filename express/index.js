e6f4b8fe415e0e6c2c0c49024bd3bbcfd5bc43c2
“Avinash_Manoj” <avinashmanoj491@gmail.com>
2025-08-21 22:02:06 +0530

    handling response object

--- express/index.js
+++ express/index.js
@@ -1,17 +1,31 @@
 import express from "express";
+import path, {dirname} from "path";
+import {fileURLToPath} from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.status(200).send("Hello World!");
 });
 
 app.get("/about", (req, res) => {
-    res.send("About Page!");
+    res.status(200).json({ message: "About Page!" });
+});
+
+app.get("/contact", (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, "contact.html"));
+});
+
+app.get("/offers", (req, res) => {
+    res.status(302).redirect("/contact");
 });
 
 app.post("/register", (req, res) => {
-    res.send("This is the register page");
+    res.status(200).send("This is the register page");
 });
 
 app.put("/update", (req, res) => {
@@ -24,5 +38,5 @@ app.delete("/delete", (req, res) => {
 
 
 app.listen(3000, () => {
-    console.log("Server started at port 3000!");
+    console.log("Server is running on http://localhost:3000");
 });
