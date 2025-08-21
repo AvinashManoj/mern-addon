a124a2bf5dcb1fe943747f471d27b864d540ef07
“Avinash_Manoj” <avinashmanoj491@gmail.com>
2025-08-21 21:12:26 +0530

    express route handlers implemented

--- express/index.js
+++ express/index.js
@@ -6,6 +6,22 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+app.get("/about", (req, res) => {
+    res.send("About Page!");
+});
+
+app.post("/register", (req, res) => {
+    res.send("This is the register page");
+});
+
+app.put("/update", (req, res) => {
+    res.send("This is the update page");
+});
+
+app.delete("/delete", (req, res) => {
+    res.send("This is the delete page");
+})
+
 
 app.listen(3000, () => {
     console.log("Server started at port 3000!");
